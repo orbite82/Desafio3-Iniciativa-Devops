23d2c186f092e802c22d68e8891fcbb5c3a3d9df
Thiago Orbite <thiagoorbite@gmail.com>
2022-06-08 17:14:17 -0300

    ajustes

--- main.tf
+++ main.tf
@@ -9,6 +9,5 @@ terraform {
 
 # Configure the DigitalOcean Provider
 provider "digitalocean" {
-  token = var.do_token
-  #"dop_v1_97893cf7f13a37b8c1e138ddb6ba5c254348a2e1ebe0a3e1228f142ab5c66dbb"
+  token = var.do_token  
 }
--- resource.tf
+++ resource.tf
@@ -16,11 +16,6 @@ resource "digitalocean_kubernetes_cluster" "k8s_iniciativa" {
 variable "do_token" {}
 variable "k8s_name" {}
 variable "region" {}
-
-#output "kube_endpoint" {
-#    value = digitalocean_kubernetes_cluster.k8s_iniciativa.endpoint
-#}
-
 resource "local_file" "kube_config" {
     content  = digitalocean_kubernetes_cluster.k8s_iniciativa.kube_config.0.raw_config
     filename = "kube_config.yaml"
